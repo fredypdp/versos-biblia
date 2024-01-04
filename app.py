3d40abf71cdacf7953d7e7df83acc5c705fb5f90
Fredy <fredrodrigues795@gmail.com>
2024-01-04 17:46:27 +0100

    Update app.py 10

--- app.py
+++ app.py
@@ -8,19 +8,20 @@ app = Flask(__name__)
 tree = ET.parse('biblia.xml')
 root = tree.getroot()
 
+
+for verse in verses:
+    if verse.text is not None:
+        print(verse.text)  # Isso imprime o texto de cada verso para verificar se está sendo extraído corretamente
+
 # Encontrar todas as tags <v> no XML
 verses = root.findall(".//v")
 verses_text = [verse.text.strip() for verse in verses if verse.text is not None]  # Extrair textos dos versos
-
-print(verse.text.strip() for verse in verses)
-print(random.choice(verses_text))
 
 # Rota para retornar um texto aleatório com a tag <v> sem remover
 @app.route('/')
 def random_verse():
     if verses_text:
         random_verse = random.choice(verses_text)  # Escolhe um verso aleatório sem remover
-        print(random_verse) 
         return random_verse
     else:
         return "Nenhum verso encontrado."
