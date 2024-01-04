d082f5c3cca0ba4acc8df5bcc0a9849b43a3ad88
Fredy <fredrodrigues795@gmail.com>
2024-01-04 20:42:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const app = express();
 
 // Lê o arquivo XML
-const xmlData = fs.readFileSync('biblia.xml');
+const xmlData = fs.readFileSync('seuarquivo.xml');
 
 // Converte o XML para objeto JavaScript
 let parsedData;
@@ -15,12 +15,13 @@ xml2js.parseString(xmlData, (err, result) => {
     return;
   }
   parsedData = result;
+  console.log(parsedData); // Verifique os dados do XML após a conversão
 });
 
 // Rota para obter um texto aleatório de uma tag v
-app.get('/randomText', (req, res) => {
-  if (!parsedData) {
-    res.status(500).json({ error: 'Erro ao processar o arquivo XML.' });
+app.get('/', (req, res) => {
+  if (!parsedData || !parsedData.usfx || !parsedData.usfx.book || !parsedData.usfx.book[0] || !parsedData.usfx.book[0].v) {
+    res.status(500).json({ error: 'Estrutura do arquivo XML não está conforme o esperado.' });
     return;
   }
 
@@ -28,8 +29,14 @@ app.get('/randomText', (req, res) => {
   const randomIndex = Math.floor(Math.random() * verses.length);
   const randomVerse = verses[randomIndex];
 
+  // Verifique se o randomVerse está definido e se possui a propriedade '_'
+  if (!randomVerse || !randomVerse['_'] || !Array.isArray(randomVerse['_']) || randomVerse['_'].length === 0) {
+    res.status(500).json({ error: 'Estrutura do verso não está conforme o esperado.' });
+    return;
+  }
+
   // Retorna o texto do verso na tag v aleatória
-  const verseText = randomVerse['_'][0]; // Correção para obter o texto do verso
+  const verseText = randomVerse['_'][0];
 
   res.json({ text: verseText });
 });
