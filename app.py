a9ce8db130f3cc2928f15a00c8dbb58916385cf6
Fredy <fredrodrigues795@gmail.com>
2024-01-04 18:56:05 +0100

    6

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from lxml import etree
 import random
@@ -6,11 +7,15 @@ app = Flask(__name__)
 
 # Função para extrair versos do arquivo XML
 def extract_verses(file):
-    with open(file, 'r', encoding='utf-8') as xml_file:
-        tree = etree.parse(xml_file)
-        verses = [elem.text for elem in tree.iterfind('.//v') if elem.text]
-        print(verses)
-        return [verse.strip() for verse in verses if verse.strip()]
+    if os.path.exists(file):
+        with open(file, 'r', encoding='utf-8') as xml_file:
+            tree = etree.parse(xml_file)
+            verses = tree.findall('.//v')
+            verses_text = [verse.text.strip() for verse in verses if verse.text is not None]
+            return [verse for verse in verses_text if verse]  # Remove textos vazios
+    else:
+        print("Arquivo não encontrado.")
+        return []
 
 verses_text = extract_verses('biblia.xml')
 random.shuffle(verses_text)
