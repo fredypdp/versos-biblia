7890ee0b4989113cf510e608169d4fe1f70bdc47
Fredy <fredrodrigues795@gmail.com>
2024-01-04 15:43:13 +0100

    Update app.py 6

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ root = tree.getroot()
 
 # Encontrar todas as tags <v> no XML
 verses = root.findall(".//v")
-print(verses)
 verses_text = [verse.text.strip() for verse in verses if verse.text is not None]  # Extrair textos dos versos
 
+print(verses_text)
+print([verse.text.strip() for verse in verses])
+print(random.choice(verses_text))
+
 # Rota para retornar um texto aleatório com a tag <v> sem remover
-@app.route('/random_verse')
+@app.route('/')
 def random_verse():
     if verses_text:
         random_verse = random.choice(verses_text)  # Escolhe um verso aleatório sem remover
